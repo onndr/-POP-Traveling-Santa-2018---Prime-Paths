511ac63616a37956ceb4ca0d5e37e00bfd87eef7
Andrii Gamalii <gamalii.andrii@gmail.com>
2024-01-10 20:37:39 +0100

    small tests refactoring

--- tests/test_route.py
+++ tests/test_route.py
@@ -1,51 +1,43 @@
 import math
-
-from src.constants import Point_t
 from src.routeoptimization.route import Route
 from src.dataprocessing.initialparse import read_cities_data
-import numpy as np
 
+n_points = 40
+cities = read_cities_data(filename="../cities.csv", restrict=n_points, primes_filename="../primes.txt")
 
-# p1 = (0, 0, 0, False)
-# p2 = (1, 1, 1, False)
-# p3 = (2, 0, 1, True)
-# p4 = (3, 1, 0, True)
-# p5 = (4, 0, 2, False)
-# p6 = (5, 1, 5, True)
-# points = [p1, p2, p3, p4, p5, p6]
-# np_points = np.array(points, dtype=Point_t)
-cities = read_cities_data(filename="../cities.csv", restrict=50, primes_filename="../primes.txt")[10:]
 
 def test_route_two_opt_simple():
+    n_iters = 1000
+    n_start_routes = 5
     route = Route(cities)
 
     assert route.graph is None
-    assert route.n_points == 40
+    assert route.n_points == n_points
     init_cost = route.best_cost
-    order, cost = route.optimize(Route.two_opt, 1000, 5)
+    order, cost = route.optimize(Route.two_opt, n_iters, n_start_routes)
     assert math.isclose(cost, route.best_cost)
     assert cost < init_cost
     print("Two opt")
-    print(init_cost, cost)
-    print("Route")
-    print(len(route.best_order))
-    print("_________")
+    print("Initial cost: ", '%.3f' % init_cost)
+    print("Post opt cost: ", '%.3f' % cost)
+    print("___________________________")
 
 
 def test_route_three_opt_simple():
+    n_iters = 1000
+    n_start_routes = 5
     route = Route(cities)
 
     assert route.graph is None
-    assert route.n_points == 40
+    assert route.n_points == n_points
     init_cost = route.best_cost
-    order, cost = route.optimize(Route.three_opt, 1000, 5)
+    order, cost = route.optimize(Route.three_opt, n_iters, n_start_routes)
     assert math.isclose(cost, route.best_cost)
     assert cost < init_cost
     print("Three opt")
-    print(init_cost, cost)
-    print("Route")
-    print(len(route.best_order))
-    print("_________")
+    print("Initial cost: ", '%.3f' % init_cost)
+    print("Post opt cost: ", '%.3f' % cost)
+    print("___________________________")
 
 
 test_route_two_opt_simple()
